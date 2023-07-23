e17d931bccf081dd4374daa5359f67e14d79400c
itsfloppyhat <itsfloppyhat@gmail.com>
2023-07-23 10:29:16 -0400

    added widgetsflutterbinding.ensureinitialized

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -15,104 +15,107 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: const Color.fromARGB(255, 20, 3, 3),
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.flutter_dash,
-                  size: 100, color: Color.fromARGB(255, 172, 237, 81)),
-              Text(
-                'Hello Again!',
-                style: GoogleFonts.bebasNeue(
-                    fontSize: 40, color: Color.fromARGB(255, 172, 237, 81)),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Let's run it back",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Color.fromARGB(255, 172, 237, 81),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.flutter_dash,
+                    size: 100, color: Color.fromARGB(255, 172, 237, 81)),
+                Text(
+                  'Hello Again!',
+                  style: GoogleFonts.bebasNeue(
+                      fontSize: 40, color: Color.fromARGB(255, 172, 237, 81)),
                 ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(244, 244, 244, 0.561),
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Username',
-                    ),
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 172, 237, 81),
-                    ),
+                SizedBox(height: 10),
+                Text(
+                  "Let's run it back",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 172, 237, 81),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(244, 244, 244, 0.561),
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Password',
+                SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(244, 244, 244, 0.561),
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 172, 237, 81),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Username',
+                      ),
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 172, 237, 81),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  child: Center(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          textStyle: const TextStyle(fontSize: 20),
-                          backgroundColor: Color.fromARGB(255, 172, 237, 81),
-                          minimumSize: Size(double.infinity, 40)),
-                      onPressed: () {},
-                      child: const Text("Sign In"),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(244, 244, 244, 0.561),
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Password',
+                      ),
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 172, 237, 81),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Not a member?',
-                    style: TextStyle(color: Color.fromARGB(255, 172, 237, 81)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: Center(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            textStyle: const TextStyle(fontSize: 20),
+                            backgroundColor: Color.fromARGB(255, 172, 237, 81),
+                            minimumSize: Size(double.infinity, 40)),
+                        onPressed: () {},
+                        child: const Text("Sign In"),
+                      ),
+                    ),
                   ),
-                  Text(
-                    ' Register now',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 172, 237, 81)),
-                  ),
-                ],
-              )
-            ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Not a member?',
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 172, 237, 81)),
+                    ),
+                    Text(
+                      ' Register now',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 172, 237, 81)),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
